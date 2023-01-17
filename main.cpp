6466e668ab560f556a9f50c4271ba4bfb67c2f7c
Dawson VanderLugt <dnv5@calvin.edu>
2023-01-16 23:35:35 -0500

    all done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 int main() {
+    float arr[20] = {};
+
+    cout << sizeof(arr[20]) << endl;
+
+    float *ptr = &arr[7];
+    *ptr = 17.771;  
+
+    cout << arr[7] << endl;
+    cout << *ptr << endl;
 
     return 0;
 }
